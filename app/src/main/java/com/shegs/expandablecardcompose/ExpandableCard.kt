7549ccc498b787fe6a8c4491f72b5baa69f2c89a
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-07-24 15:34:43 +0100

    Increased compileSdk to 34, minSdk to 25 and targetSdk to 34

--- app/src/main/java/com/shegs/expandablecardcompose/ExpandableCard.kt
+++ app/src/main/java/com/shegs/expandablecardcompose/ExpandableCard.kt
@@ -3,16 +3,30 @@ package com.shegs.expandablecardcompose
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material3.ContentAlpha
 import com.shegs.expandablecardcompose.ui.theme.Shapes
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -33,6 +47,33 @@ fun ExpandableCard(){
         shape = Shapes.medium,
         onClick = { expandedState = !expandedState}
     ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "My Title",
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = FontWeight.Bold
+                )
+
+                IconButton(
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium),
+                    onClick = { expandedState = !expandedState}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Drop Down Arrow")
+                }
+            }
+
+        }
 
     }
 }
